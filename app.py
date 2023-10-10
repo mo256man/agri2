ec9277e07f06a9ee259a76e814adfbb51a796861
mo256man <48610559+mo256man@users.noreply.github.com>
2023-10-10 15:22:34 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import random
 from time import sleep
 # from gpiozero import MCP3004
 
+light_pins = [26, 19, 13, 6, 5]     # 5個の光センサーの状態を取得するラズパイのGPIOピン
+
 nagoya = {"place": "名古屋",
             "lat": 35.1667,
             "lon": 136.9167,
@@ -46,40 +48,75 @@ def getEphem():
     except Exception as e:
         message = str(e)
         dict = {"answer": message}  # エラーメッセージ
-    return json.dumps(dict)             # 辞書をJSONにして返す
+    return json.dumps(dict)         # 辞書をJSONにして返す
+
 
 # バッテリー電圧
 @app.route("/getBatt", methods=["POST"])
 def getBatt():
     if request.method == "POST":
         is_test = request.form["isTest"]
-        print(is_test)
         dict = {}
         if is_test:
             dict["ana3"] = random.randint(0, 100)
             dict["ana0"] = random.randint(0, 100)
             sleep(3)
+            print(f"電圧（トライ）:{dict}")
         else:
             ana3 = analog_read(ch=3)
             ana0 = analog_read(ch=0)
             dict["ana3"] = int(ana3*100)
             dict["ana0"] = int(ana0*100)
-        print(dict)
+            print(f"電圧（本番）:{dict}")
         return json.dumps(dict)
 
 
-"""
-@app.route("/call_from_ajax", methods = ["POST"])
-def callfromajax():
+# 温湿度計
+@app.route("/getHumi", methods=["POST"])
+def getHumi():
     if request.method == "POST":
-        # ここにPythonの処理を書く
-        try:
-            pass
-        except Exception as e:
-            message = str(e)
-        dict = {"answer": message}      # 辞書
-    return json.dumps(dict)             # 辞書をJSONにして返す
-"""
+        is_test = request.form["isTest"]
+        dict = {}
+        if is_test:
+            dict["temp"] = random.randint(10, 40)
+            dict["humi"] = random.randint(0, 100)
+            sleep(3)
+            print(f"温湿度（トライ）:{dict}")
+        else:
+            #result = sensor.read()
+            result = False
+            if result.is_valid():
+                dict["temp"] = round(result.temperature, 1) # 温度 小数第一位まで
+                dict["humi"] = round(result.humidity, 1)    # 湿度 小数第一位まで
+            else:
+                dict["temp"] = "N/A"
+                dict["humi"] = "N/A"
+            print(f"温湿度（本番）:{dict}")
+        return json.dumps(dict)
+
+
+# 光センサー
+@app.route("/getLight", methods=["POST"])
+def getLight():
+    if request.method == "POST":
+        is_test = request.form["isTest"]
+        lights = []
+        dict = {}
+        if is_test:
+            for _ in light_pins:
+                lights.append(random.choice([True, False]))
+            dict["lights"] = lights
+            sleep(3)
+            print(f"光センサー（トライ）:{dict}")
+        else:
+            for pin in light_pins:
+                lights.append(GPIO.input(pin))
+            dict["lights"] = lights
+            sleep(3)
+            print(f"光センサー（本番）:{dict}")
+        return json.dumps(dict)
+
 
 if __name__ == "__main__":
+    # app.run(host="0,0,0,0", port=5000, debug=True)
     app.run(debug=True)
--- static/scripts/myscript.js
+++ static/scripts/myscript.js
@@ -1,12 +1,15 @@
 // グローバル変数
-let isAuto =true;       // 自動か各個か
-let isTry = true;       // トライか本番か
-let isLED = false;      // 育成LEDを光らせるか
-let isForce = false;    // LEDを強制的にオンオフさせるか光センサーで制御するか
+let isAuto =true;           // 自動か各個か
+let isHumiTry = true;       // 温湿度計がトライか本番か
+let isBattTry = true;       // バッテリ電圧計がトライか本番か
+let isLightTry = true;      // 光センサーがトライか本番か
+let isLEDTry = true;        // 育成LEDがトライか本番か
+let isLED = false;          // 育成LEDを光らせるか
+let isForce = false;        // LEDを強制的にオンオフさせるか光センサーで制御するか
 let tab = "main";
 let lamps = [0, 0, 0, 0, 1];
 const pins = ["26", "19", "13", "6", "5"];
-let temp, humi
+let temp, humi              // 温度と湿度
 let vp, volt, capa, BT, Ah, pow, cnt, voltage, BTcnt, charge, maxwh, maxv
 let redp, yellowp, totalwh
 let sunrise_time, sunset_time
@@ -22,14 +25,13 @@ let bp;
 $(async function() {
     // 読み込み完了後に一度だけ実行する関数
     getNow();
-    showTry();
     //tab = getTab();
     clearMsg();
     addMsg(time+"　開始")
     await getEphem();
     await getBattSetting();
-    get_or_try_Humi();
-    get_or_try_Volt();
+    await getBatt(true);
+    await getHumi(true);
 
     setInterval(showTime, 1000);
     get_or_try_Sunlight();
@@ -84,10 +86,30 @@ $(async function() {
         }
     })
 
+
     // トライボタン切り替え
     $(".btnTry").on('click', function(){
-        isTry = !isTry;
-        showTry();
+        const btnid = $(this).attr("id");
+        var bool = false;
+        switch (btnid) {
+            case "HumiTry":
+                isHumiTry = ! isHumiTry;
+                bool = isHumiTry;
+                break;
+            case "BattTry":
+                isBattTry = ! isBattTry;
+                bool = isBattTry;
+                break;
+            case "LightTry":
+                isLightTry = ! isLightTry;
+                bool = isLightTry;
+                break;
+            case "LEDTry":
+                isLEDTry = ! isLEDTry;
+                bool = isLEDTry;
+                break;
+        }
+        showTryBtn("#"+btnid, bool);
     })
 
 });
@@ -119,15 +141,16 @@ async function showTime() {
 
     if (isAuto) {
         // 60分ごとに温湿度を更新する
-        if (s==0 && m==0) {
-            addMsg(time + "　温湿度更新");
-            get_or_try_Humi();
+        if (s==20) {
+            getHumi(isHumiTry);
         }
+
+        
         // 10分ごとバッテリ残容量を更新する
         if (s==10) {
             addMsg(time + "　バッテリ残容量更新");
-            bp = await getBatt(isTry);
-            console.log("bp=" + bp);
+            bp = await getBatt(isBattTry);
+            
         }
         // 1分ごとに光センサーを更新する　ただし自動制御中のみ
         if (! isForce) {
@@ -170,19 +193,18 @@ async function showTime() {
 
 }
 
+
+//////////////////////////////////////////////////////////////////////
 // トライの状態を表示する関数
-function showTry() {
-    const elm = $("#btnTry");
-    if (isTry) {
-        $("body").css("background-color", "yellow");
+function showTryBtn(btnid, bool) {
+    const elm = $(btnid);
+    if (bool) {
         elm.css("background-color", "pink");
         elm.text("トライ");
     } else {
-        $("body").css("background-color", "white");
         elm.css("background-color", "yellow");
         elm.text("本番");
     }
-    //get_or_try_Humi();
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -202,46 +224,28 @@ function clearMsg() {
 }
 
 //////////////////////////////////////////////////////////////////////
-// 本番かトライかを判定して温湿度を取得する
-async function get_or_try_Humi() {
-    if (isTry) {
-        await tryHumi().then( result => {
-            temp = result[0];
-            humi = result[1];
-        });
-    } else {
-        await getHumi().then( result => {
-            temp = result[0];
-            humi = result[1];
-        });
-    }
-    $("#temp").text(temp + "℃");
-    $("#humi").text(humi + "％");
-}
-
-// 温湿度（トライ）
-async function tryHumi() {
-    var temp = 20 + Math.floor(Math.random()*10);
-    var humi = 50 + Math.floor(Math.random()*30);
-    return [temp, humi]
-}
-
-// 温湿度（本番）
-async function getHumi() {
+//    温湿度
+//////////////////////////////////////////////////////////////////////
+async function getHumi(isTest) {
     await $.ajax("/getHumi", {
-        type: "POST",
+        type: "post",
+        data: {"isTest": isTest},               // テストか本番かのbool値をisTestとして送る
     }).done(function(data) {
-        var dict = JSON.parse(data);
-        if (dict["temp"] != "N/A") {
+        const dict = JSON.parse(data);
+        if (dict["temp"] != "N/A") {            // センサー値取得できていたら
             temp = dict["temp"];
-        }
-        if (dict["humi"] != "N/A") {
             humi = dict["humi"];
+            $("#temp").text(temp + "℃");
+            $("#humi").text(humi + "％");
+            addMsg(time + "　温湿度更新");
+        } else {                                // センサー値取得できなかったら
+            console.log("温湿度　センサー失敗");
         }
-    }).fail(function() {
-        console.log("温湿度取得失敗");
+    }).fail(function() {                        // ajaxのリターン失敗したら更新しない
+        console.log("温湿度　通信失敗");
     });
-};
+}
+
 
 
 //////////////////////////////////////////////////////////////////////
@@ -249,7 +253,7 @@ async function getHumi() {
 //////////////////////////////////////////////////////////////////////
 // 本番かトライかを判定して日光を取得する
 async function get_or_try_Sunlight() {
-    if (isTry) {
+    if (isLightTry) {
         await trySunlight().then( result => {
             lamps = result;
         });
@@ -308,6 +312,25 @@ async function getSunlight() {
         console.log("温湿度取得失敗");
     });
 };
+
+
+async function getLight(isTest) {
+    await $.ajax("/getLight", {
+        type: "post",
+        data: {"isTest": isTest},               // テストか本番かのbool値をisTestとして送る
+    }).done(function(data) {
+        const dict = JSON.parse(data);
+        if (dict["light"] != "N/A") {            // センサー値取得できていたら
+            temp = dict["temp"];
+            $("#humi").text(humi + "％");
+            addMsg(time + "　光センサー更新");
+        } else {                                // センサー値取得できなかったら
+            console.log("光センサー　センサー失敗");
+        }
+    }).fail(function() {                        // ajaxのリターン失敗したら更新しない
+        console.log("光センサー　通信失敗");
+    });
+}
 
 
 // 育成LEDを光らせる
@@ -378,7 +401,7 @@ async function getEphem() {
         $("#moon_image").attr("src", dict["moon_image"]);
 
         // 日の出日の入り時刻から育成LED点灯消灯の時刻を計算する
-        morning_light_time = dayjs(today+" "+sunrise_time).add(-1.5, "h").format("HH:mm");
+        morning_light_time = dayjs(today+" "+sunrise_time).add(1.5, "h").format("HH:mm");
         evening_light_time = dayjs(today+" "+sunset_time).add(1.5, "h").format("HH:mm");
         $("#morning_light_time").text(morning_light_time);
         $("#evening_light_time").text(evening_light_time);
@@ -389,6 +412,8 @@ async function getEphem() {
 };
 
 
+//////////////////////////////////////////////////////////////////////
+//    バッテリ電圧
 //////////////////////////////////////////////////////////////////////
 // バッテリーの設定を取得する関数
 async function getBattSetting() {
@@ -412,51 +437,13 @@ async function getBattSetting() {
     });
 };
 
-// 本番かトライかを判定して電圧を取得する
-function get_or_try_Volt() {
-    if (isTry) {
-        vp = tryVolt();
-    } else {
-        vp = getVolt();
-    }
-    showBatt();
-}
-
-// 電圧（トライ）
-function tryVolt() {
-    vp = Math.floor(Math.random()*100);
-    return vp
-}
-
-// 電圧（本番）
-async function getVolt() {
-    $("#humi_time").text(time);
-    $("#temp").text(30 + "℃");
-    $("#humi").text(60 + "％");
-    await $.ajax("/getHumi", {
-        type: "POST",
-    }).done(function(data) {
-        var dict = JSON.parse(data);
-        if (dict["temp"] != "N/A") {
-            $("#humi_time").text(time);
-            //console.log(dict["temp"]);
-            $("#temp").text(dict["temp"] + "℃");
-        }
-        if (dict["humi"] != "N/A") {
-            //console.log(dict["humi"]);
-            $("#humi").text(dict["humi"] + "％");
-        }
-    }).fail(function() {
-        console.log("温湿度取得失敗");
-    });
-};
 
 //
 async function getBatt(isTest) {
     let bat = 0;
     await $.ajax("/getBatt", {
         type: "post",
-        data: {"isTest": isTest},              // 連想配列をPOSTする
+        data: {"isTest": isTest},              // テストか本番かのbool値をisTestとして送る
     }).done(function(data) {
         const dict = JSON.parse(data);
         bat = dict["ana3"];
@@ -464,23 +451,23 @@ async function getBatt(isTest) {
         console.log("バッテリー電圧取得失敗");
     });
     console.log("bat="+bat);
+    showBatt(bat);
     return bat
 }
 
 
-
 // バッテリーの設定を表示する関数
-function showBatt() {
+function showBatt(bp) {
     maxwh = Math.trunc(Ah * voltage * BTcnt / 2);
     maxv = voltage;
-    wh = Math.trunc(maxwh*vp/100);
-    volt = Math.trunc(maxv*vp/100);
+    wh = Math.trunc(maxwh*bp/100);
+    volt = Math.trunc(maxv*bp/100);
     charge = Math.trunc(charge*.3);
     totalwh = Math.trunc(wh+charge);
     need_next = Math.trunc(pow*cnt*1);
     redp = Math.trunc(red3h/maxwh*100);
 
-    $("#vp").text(vp);          // パーセント
+    $("#vp").text(bp);          // パーセント
     $("#wh").text(wh);
     $("#maxwh").text(maxwh);
     $("#volt").text(volt);
@@ -489,247 +476,9 @@ function showBatt() {
     $("#calc_totalwh").html(wh+"Wh+"+charge+"Wh="+totalwh+"Wh");
     $("totalwh").html(totalwh);
     $("#calc_next").html(pow+"W*"+cnt+"*1h="+need_next+"Wh");
-    $("#meter-bar").css("width", vp+"%");
+    $("#meter-bar").css("width", bp+"%");
     $("#bar_red").css("width", redp+"%")
+    $("#bar_yellow").css("width", redp+"%")
+    $("#bar_green").css("width", redp+"%")
 }
 
-
-
-/* 
-$(function() {
-    // 読み込み完了後に一度だけ実行する関数
-    getNow();
-    startProcess();
-    getBattSetting();
-    setInterval(showTime, 1000);
-    showState();
-    showLamps();
-    showLedLamp();
-    showTry();
-
-    // 温湿度を取得
-    get_or_try_Humi();
-    get_or_try_Volt();
-
-    // フラグを反転したのち状態を表示する関数
-    $("#swAuto").on('click', function(){
-        isAuto = !isAuto;
-        showState();
-        showLedLamp();
-    })
-
-    // ランプ全点灯ボタンを押す
-    $("#btnAllLight").mousedown(function(){
-        if (! isAuto) {
-            $("#imgLight").attr("src", "static/images/btn_push.png");
-            for (i=0; i<5; i++){
-                $("#lamp" + i).css("color","red");
-                $("#lamp_led").css("color","red");
-            }
-        }
-    })
-
-    // ランプ全点灯ボタンを離す
-    $("#btnAllLight").mouseup(function(){
-        if (! isAuto) {
-            $("#imgLight").attr("src", "static/images/btn_normal.png");
-            showLamps();
-            showLedLamp();
-        }
-    })
-
-    // LED強制点灯ボタンを押す
-    $("#btnLedOn").mousedown(function(){
-        if (! isAuto) {
-            $("#btnLedOn").attr("src", "static/images/btn_push.png");
-            $("#imgLed").attr("src", "static/images/led_on.png");
-            $("#lamp_led").css("color", "red");
-        }
-    })
-
-    // LED強制点灯ボタンを離す
-    $("#btnLedOn").mouseup(function(){
-        if (! isAuto) {
-            $("#btnLedOn").attr("src", "static/images/btn_normal.png");
-            $("#imgLed").attr("src", "static/images/led_off.png");
-            showLedLamp();
-        }
-    })
-
-    // トライボタン切り替え
-    $(".btnTry").on('click', function(){
-        isTry = !isTry;
-        showTry();
-    })
-
-})
-
-
-
-
-// 起動時and日付が変わったときの処理
-async function startProcess() {
-    const result = await getEphem();
-    clearMsg();
-    var txt = time + " 【開始】<br>";
-    txt += "　今日は" + now.format("M月D日") + "<br>";
-    txt += "　日の出は" + sunrise_time + "<br>";
-    txt += "　→育成LEDを" + morning_light_time.format("HH:mm") + "に点灯<br>";
-    txt += "　日の入りは" + sunset_time + "<br>";
-    txt += "　→育成LEDを" + evening_light_time.format("HH:mm") + "まで点灯<br>";
-    addMsg(txt);
-}
-
-
-//////////////////////////////////////////////////////////////////////
-
-
-}
-
-// トライの状態を表示する関数
-function showTry() {
-    const elm = $("#btnTry");
-    if (isTry) {
-        elm.css("background-color", "pink");
-        elm.text("トライ");
-    } else {
-        elm.css("background-color", "yellow");
-        elm.text("本番");
-    }
-    get_or_try_Humi();
-}
-
-
-// フラグの状態を表示する関数
-function showState() {
-    var strAuto = "";
-    var imgSw = "";
-    if (isAuto) {
-        strAuto = "自動";
-        imgSw = "sw_l.png";
-    } else {
-        strAuto = "各個";
-        imgSw = "sw_r.png";
-    };
-    $("#stateAuto").text(strAuto);
-    $("#imgAuto").attr("src", "static/images/" + imgSw );
-}
-
-// 光センサーの状態を表示する関数
-function showLamps() {
-    $.each(lamps,function(index,val){
-        var color="";
-        if (val) {
-            color = "red";
-        } else {
-            color = "gray";
-        }
-        $("#lamp" + index).css("color",color);
-    });
-}
-
-// 育成LEDをの状態を表示する関数
-function showLedLamp() {
-    var color="";
-    if (isLED) {
-        color = "red";
-    } else {
-        color = "gray";
-    }
-    $("#lamp_led").css("color",color);
-}
-
-
-
-
-
-
-
-
-// 本番かトライかを判定して温湿度を取得する
-function get_or_try_Humi() {
-    clearInterval(humiID);                      // 既存のインターバルをクリア
-    if (isTry) {
-        tryHumi();
-        //humiID = setInterval(tryHumi, 1000);    // 1秒毎にtryHumiを実行
-    } else {
-        getHumi();                              // 最初の1回を実行
-        //humiID = setInterval(getHumi, 60000);   // 10分ごとにを実行
-    }
-}
-
-// 温湿度（トライ）
-function tryHumi() {
-    $("#humi_time").text(time);
-    const temp = 20 + Math.floor(Math.random()*10);
-    const humi = 50 + Math.floor(Math.random()*30);
-    $("#temp").text(temp + "℃");
-    $("#humi").text(humi + "％");
-}
-
-// 温湿度（本番）
-function getHumi() {
-    $.ajax("/getHumi", {
-        type: "POST",
-    }).done(function(data) {
-        var dict = JSON.parse(data);
-        if (dict["temp"] != "N/A") {
-            $("#temp").text(dict["temp"] + "℃");
-        }
-        if (dict["humi"] != "N/A") {
-            $("#humi").text(dict["humi"] + "％");
-        }
-    }).fail(function() {
-        console.log("温湿度取得失敗");
-    });
-};
-
-
-// 本番かトライかを判定して電圧を取得する
-function get_or_try_Volt() {
-    clearInterval(voltID);                      // 既存のインターバルをクリア
-    if (isTry) {
-        tryVolt();
-        //voltID = setInterval(tryVolt, 1000);    // 1秒毎にtryHumiを実行
-    } else {
-        getHumi();                              // 最初の1回を実行
-        //voltID = setInterval(getVolt, 60000);  // 10分ごとにを実行
-    }
-}
-
-// 電圧（トライ）
-function tryVolt() {
-    const volt = Math.floor(Math.random()*100);
-    $("#volt").text(volt + "%");
-    $("#wh").text(Math.trunc(maxwh*volt/100));
-    $("#v").text(Math.trunc(voltage*volt/100));
-    $("#meter-bar").css("width", volt + "%");
-    const totalwh = Math.trunc(wh + charge*0.3);
-    $("#totalwh").text(Math.trunc(maxwh*volt/100)+"Wh+"+charge+"Wh*0.3="+totalwh+"Wh");
-    showBatt();
-}
-
-// 電圧（本番）
-function getVolt() {
-    $("#humi_time").text(time);
-    $("#temp").text(30 + "℃");
-    $("#humi").text(60 + "％");
-    $.ajax("/getHumi", {
-        type: "POST",
-    }).done(function(data) {
-        var dict = JSON.parse(data);
-        if (dict["temp"] != "N/A") {
-            $("#humi_time").text(time);
-            //console.log(dict["temp"]);
-            $("#temp").text(dict["temp"] + "℃");
-        }
-        if (dict["humi"] != "N/A") {
-            //console.log(dict["humi"]);
-            $("#humi").text(dict["humi"] + "％");
-        }
-    }).fail(function() {
-        console.log("温湿度取得失敗");
-    });
-};
-
-*/
